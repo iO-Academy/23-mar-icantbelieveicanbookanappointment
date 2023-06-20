e2d584e6dab59d34e99b5b8a1010c9bd4551ba29
Adam Young <adamyoungy678@gmail.com>
2023-06-20 16:56:18 +0100

    fix: fixed the logic to fetch whenever the appointments update

--- src/components/AppointmentSchedule/index.js
+++ src/components/AppointmentSchedule/index.js
@@ -1,43 +1,38 @@
-import React, { useEffect, useState } from "react";
-import './_appointmentSchedule.scss'
 import BASE_URL from "../../settings";
+
+import React, { useEffect, useState } from "react";
+import './_appointmentSchedule.scss';
 
 const AppointmentSchedule = ({ appointments }) => {
     const [patientNames, setPatientNames] = useState({});
 
     useEffect(() => {
+        const fetchPatientNames = async () => {
+            const promises = appointments.map(appointment =>
+                fetch(`${BASE_URL}patient/${appointment.patientId}`)
+                    .then(response => response.json())
+                    .then(data => ({
+                        id: appointment.patientId,
+                        name: `${data.data.first_name} ${data.data.last_name}`
+                    }))
+            );
+
+            const resolvedPatientNames = await Promise.all(promises);
+
+            const patientNameMap = resolvedPatientNames.reduce(
+                (map, patient) => ({ ...map, [patient.id]: patient.name }),
+                {}
+            );
+
+            setPatientNames(patientNameMap);
+        };
+
         fetchPatientNames();
-    }, []);
-
-    const fetchPatientNames = async () => {
-        const promises = appointments.map(appointment =>
-            fetch(`${BASE_URL}patient/${appointment.patientId}`)
-                .then(response => response.json())
-                .then(data => ({ id: appointment.patientId, name: data.data }))
-        );
-
-        const resolvedPromises = await Promise.all(promises);
-        const names = resolvedPromises.reduce((acc, { id, name }) => {
-            acc[id] = name;
-            return acc;
-        }, {});
-
-        setPatientNames(names);
-    };
+    }, [appointments]);
 
     if (appointments.length === 0) {
         return <div>No appointments available.</div>;
     }
-
-    // Helper function to get the full name of a patient using their ID
-    const getFullName = (patientId) => {
-        const patient = patientNames[patientId];
-        if (patient) {
-            const { first_name, last_name } = patient;
-            return `${first_name} ${last_name}`;
-        }
-        return ''; // Return an empty string if the patient is not found
-    };
 
     const sortedAppointments = appointments.sort((a, b) => a.time - b.time);
 
@@ -46,7 +41,9 @@ const AppointmentSchedule = ({ appointments }) => {
             {sortedAppointments.map((appointment, index) => (
                 <div key={index} className="appointment">
                     <div className="time">{appointment.time}:00</div>
-                    <div className="patient-name">{getFullName(appointment.patientId)}</div>
+                    <div className="patient-name">
+                        {patientNames[appointment.patientId] || "Loading..."}
+                    </div>
                 </div>
             ))}
         </div>
@@ -54,3 +51,5 @@ const AppointmentSchedule = ({ appointments }) => {
 };
 
 export default AppointmentSchedule;
+
+
