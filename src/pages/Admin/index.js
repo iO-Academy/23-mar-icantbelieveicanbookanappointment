9b47c8809c3f949602f317002c54671997d20bb8
Adam Young <adamyoungy678@gmail.com>
2023-06-20 15:42:18 +0100

    feat: correctly fetching appointments

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,19 +1,40 @@
 import DateInput from "../../components/Inputs/DateInput";
 import AppointmentSchedule from "../../components/AppointmentSchedule";
 import './_admin.scss'
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import BASE_URL from "../../settings";
 
 const Admin = () => {
-    const [selectedDate, setSelectedDate] = useState("")
+    const [selectedDate, setSelectedDate] = useState("");
+    const [appointments, setAppointments] = useState([]);
+
     useEffect(() => {
-        console.log(selectedDate)
-    }, [selectedDate])
+        if (selectedDate) {
+
+            fetch(`${BASE_URL}appointments/${selectedDate}/2`)
+                .then(response => {
+                    if (response.ok) {
+                        console.log("fetched appointments")
+                        return response.json();
+                    } else {
+                        throw new Error("Failed to fetch appointments");
+                    }
+                })
+                .then(data => {
+                    setAppointments(data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    }, [selectedDate]);
+
     return (
         <div className="container">
-            <DateInput inputLabel={"Date:"} setSelectedDate={setSelectedDate}/>
-            <AppointmentSchedule />
+            <DateInput inputLabel={"Date:"} setSelectedDate={setSelectedDate} />
+            <AppointmentSchedule appointments={appointments} />
         </div>
-    )
+    );
 }
 
-export default Admin
+export default Admin;
