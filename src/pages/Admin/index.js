de4287bb44f8d918ed6af651e64c15d4124946e5
Adam Young <adamyoungy678@gmail.com>
2023-06-20 15:34:58 +0100

    feat: added a use effect and edited styling for admin page

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,10 +1,13 @@
 import DateInput from "../../components/Inputs/DateInput";
 import AppointmentSchedule from "../../components/AppointmentSchedule";
 import './_admin.scss'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const Admin = () => {
     const [selectedDate, setSelectedDate] = useState("")
+    useEffect(() => {
+        console.log(selectedDate)
+    }, [selectedDate])
     return (
         <div className="container">
             <DateInput inputLabel={"Date:"} setSelectedDate={setSelectedDate}/>
