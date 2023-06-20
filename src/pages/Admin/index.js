f3723f32c8b6696dfb4c6343cf64f1ef2b2506f9
Adam Young <adamyoungy678@gmail.com>
2023-06-20 15:33:42 +0100

    feat: added styling to admin page and added selected data state + prop

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,11 +1,13 @@
 import DateInput from "../../components/Inputs/DateInput";
 import AppointmentSchedule from "../../components/AppointmentSchedule";
+import './_admin.scss'
+import {useState} from "react";
 
 const Admin = () => {
-
+    const [selectedDate, setSelectedDate] = useState("")
     return (
-        <div >
-            <DateInput />
+        <div className="container">
+            <DateInput inputLabel={"Date:"} setSelectedDate={setSelectedDate}/>
             <AppointmentSchedule />
         </div>
     )
