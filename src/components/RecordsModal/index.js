a386cb97e41fd89c48dc54e1831e8ce93bbd79ff
Adam Young <adamyoungy678@gmail.com>
2023-06-22 11:24:22 +0100

    feat: mapped over the patient record and displayed the details

--- src/components/RecordsModal/index.js
+++ src/components/RecordsModal/index.js
@@ -2,33 +2,22 @@ import React, {useEffect, useState} from "react"
 import BASE_URL from "../../settings";
 // import "./RecordsModal.scss"
 
-const RecordsModal = ({patientId, patientName, closeRecordsModal }) => {
-    const [patientRecord, setPatientRecord] = useState([])
+const RecordsModal = ({ patientId, patientName, closeRecordsModal }) => {
+    const [patientRecord, setPatientRecord] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    let tableData = 'Loading...'
-
-        // USE EFFECT TO PERFORM A GET REQUEST TO GET THE RECORDS
+    // USE EFFECT TO PERFORM A GET REQUEST TO GET THE RECORDS
     useEffect(() => {
         const fetchPatientRecord = async () => {
-                fetch(`${BASE_URL}record/${patientId}`)
-                    .then(response => response.json())
-                    .then((data) => {
-                        setPatientRecord(data.data)
-                    }
-            )
-
-        }
-        fetchPatientRecord()
-    }, [])
-
-    useEffect(() => {
-        if (patientRecord.length === 0) {
-            tableData = `<p>This patient has no record<p>`
-        } else {
-            tableData = `<p>This patient has ${patientRecord.length} records<p>`
-            tableData = <ul><li></li></ul>
-        }
-    }, [patientRecord])
+            fetch(`${BASE_URL}record/${patientId}`)
+                .then(response => response.json())
+                .then((data) => {
+                    setPatientRecord(data.data);
+                    setIsLoading(false);
+                });
+        };
+        fetchPatientRecord();
+    }, [patientId]);
 
     return (
         <div className="modal-content">
@@ -39,11 +28,31 @@ const RecordsModal = ({patientId, patientName, closeRecordsModal }) => {
                 <h1 className="modal-title">{patientName}'s Records</h1>
             </div>
             <div className="modal-body">
-                    {tableData}
-                {/* Display patient records here */}
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : patientRecord.length === 0 ? (
+                    <p>This patient has no record</p>
+                ) : (
+                    <>
+                        <p>This patient has {patientRecord.length} records</p>
+                        <ul>
+                            {patientRecord.map((record) => (
+                                <li key={record.id}>
+                                    {/* Display record information here */}
+                                    <p>Doctor: {record.doctor}</p>
+                                    <p>Time: {record.time}</p>
+                                    <p>Date: {record.date}</p>
+                                    <p>Reason: {record.reason}</p>
+                                    <p>Notes: {record.notes}</p>
+                                    <p>Prescriptions: {record.prescriptions}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default RecordsModal
+export default RecordsModal;
